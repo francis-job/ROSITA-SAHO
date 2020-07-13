0cc9505fe61ecc95f740037c3f59b6c6d11bf182
FRANCIS JOB <48465208+francis-job@users.noreply.github.com>
2020-07-14 01:03:09 +0530

    Add files via upload

---
+++ ROTARY_ENCODER/ROTARY_ENCODER.ino
@@ -0,0 +1,72 @@
+// Rotary Encoder Inputs
+#define CLK PB6
+#define DT PB7
+#define SW PB8
+
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir ="";
+unsigned long lastButtonPress = 0;
+
+void setup() {
+  
+  // Set encoder pins as inputs
+  pinMode(CLK,INPUT);
+  pinMode(DT,INPUT);
+  pinMode(SW, INPUT_PULLUP);
+
+  // Setup Serial Monitor
+  Serial.begin(9600);
+
+  // Read the initial state of CLK
+  lastStateCLK = digitalRead(CLK);
+}
+
+void loop() {
+  
+  // Read the current state of CLK
+  currentStateCLK = digitalRead(CLK);
+
+  // If last and current state of CLK are different, then pulse occurred
+  // React to only 1 state change to avoid double count
+  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+
+    // If the DT state is different than the CLK state then
+    // the encoder is rotating CW so decrement
+    if (digitalRead(DT) != currentStateCLK) {
+      counter ++;
+      currentDir ="CW";
+    } else {
+      // Encoder is rotating CCW so increment
+      counter --;
+      currentDir ="CCW";
+    }
+
+    Serial.print("Direction: ");
+    Serial.print(currentDir);
+    Serial.print(" | Counter: ");
+    Serial.println(counter);
+  }
+
+  // Remember last CLK state
+  lastStateCLK = currentStateCLK;
+
+  // Read the button state
+  int btnState = digitalRead(SW);
+
+  //If we detect LOW signal, button is pressed
+  if (btnState == LOW) {
+    //if 50ms have passed since last LOW pulse, it means that the
+    //button has been pressed, released and pressed again
+    if (millis() - lastButtonPress > 50) {
+      Serial.println("Button pressed!");
+    }
+
+    // Remember last button press event
+    lastButtonPress = millis();
+  }
+
+  // Put in a slight delay to help debounce the reading
+  delay(1);
+}
